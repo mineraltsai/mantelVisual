59849a5321cee6e2420e6e427db7a15298c59252
mineraltsai <39892594@qq.com>
2022-09-17 17:58:44 +0800

    12

---
+++ R/mantelVisual-package.R
@@ -0,0 +1,10 @@
+##' If you use mantelVisual in published research, please cite:
+##' Minglang Cai, Wenjie Hui.
+##' Dietary Haematococcus pluvialis promotes growth of red swamp crayfish Procambarus clarkii (Girard, 1852) via positive regulation of the gut microbial co-occurrence network.
+##' Aquaculture January 2022, 551(1):737900, DOI: 10.1016/j.aquaculture.2022.737900
+##'
+##' @docType package
+##' @name mantelVisual
+##' @aliases mantelVisual package-mantelVisual
+NULL
+
